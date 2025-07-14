b6b4226dff6b703057562e50344e4465b91208f9
xRaisenx <jpm.onestop@gmail.com>
2025-07-14 11:30:33 +0800

    Revert "Integrate and customize boilerplate code"

--- .backup/current-app/app/components/common/Button.tsx
+++
@@ -1,9 +0,0 @@
-import type { ButtonProps } from '@shopify/polaris';
-import { Button as PolarisButton } from '@shopify/polaris';
-import React from 'react';
-
-export type CommonButtonProps = ButtonProps;
-
-export const Button: React.FC<CommonButtonProps> = ({ children, onClick, ...rest }) => {
-  return <PolarisButton onClick={onClick} {...rest}>{children}</PolarisButton>;
-};
